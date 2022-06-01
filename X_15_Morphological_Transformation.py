e46a6fa98b206e3aa41d572faf5c57206130e247
SahruzRiyad <48251314+SahruzRiyad@users.noreply.github.com>
2022-06-01 21:50:16 +0600

    Add files via upload

--- X_15_Morphological_Transformation.py
+++ X_15_Morphological_Transformation.py
@@ -3,16 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = cv2.imread('./Image/smarties.png',0)
-mask = cv2.imread('./Image/j.png',0)
-
-#_,mask = cv2.threshold(img,220,255,cv2.THRESH_BINARY_INV)
+_,mask = cv2.threshold(img,220,255,cv2.THRESH_BINARY_INV)
 kernal = np.ones((2,2),np.uint8)
 dilation = cv2.dilate(mask,kernal,iterations=2)
 erosion = cv2.erode(mask,kernal,iterations=4) 
 opening = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernal)
-closing = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernal)
-titles = ['Image' , 'Mask' , 'Dilation','Erosion','Opening' , 'Closing']
-images = [img , mask , dilation , erosion , opening , closing]
+titles = ['Image' , 'Threshold' , 'Dilation','Erosion','Opening']
+images = [img , mask , dilation , erosion , opening]
 
 for  i in range(len(images)):
     plt.subplot(2,3,i+1)
